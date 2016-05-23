91947adf5224e81f0339cfec15024bd456e01055
jemanjarres <jemanjarres@092634-ddocent-o.local>
2016-05-23 14:16:55 -0500

    Fixing database again

--- includes/configuracion.php
+++ includes/configuracion.php
@@ -1,9 +1,9 @@
 <?php
 $mysqli = mysqli_connect(
     getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
-    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    'admin4zRpBek',  
+    'i5GRBfq1eHJR',
+    'sistelemetria',
     getenv('OPENSHIFT_MYSQL_DB_PORT')
 );
 ?>
