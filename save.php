62fa08628ffe5bec8123319eecb5994e2cf0695d
Jose Manjarres <josemanjarrez92@gmail.com>
2016-05-23 20:46:17 -0500

    Adding special condition for Nanode

--- save.php
+++ save.php
@@ -1,11 +1,22 @@
 <?php
   if(!empty($_GET['iduser']) && !empty($_GET['idloc']) && !empty($_GET['idsens']) && !empty($_GET['valor'])){
     if(isset($_GET['iduser']) && isset($_GET['idloc']) && isset($_GET['idsens']) && isset($_GET['valor'])){
-	include 'includes/configuracion.php';
-	$sentencia = "INSERT INTO registros (reg_iduser,reg_idloc,reg_idsens,reg_valor,reg_fechahora) VALUES(".$_GET['iduser'].",".$_GET['idloc'].",".$_GET['idsens'].",".$_GET['valor'].",CURRENT_TIMESTAMP)";
-	mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); 
-	$res = mysqli_query($mysqli,$sentencia);
-	echo "yay";
+      if($_GET['iduser']==7){
+	  include 'includes/configuracion.php';
+	  $sentencia = "INSERT INTO registros (reg_iduser,reg_idloc,reg_idsens,reg_valor,reg_fechahora) VALUES(".$_GET['iduser'].",".$_GET['idloc'].",".$_GET['idsens'].",".$_GET['valor'].",CURRENT_TIMESTAMP)";
+	  $sentencia2 = "INSERT INTO registros (reg_iduser,reg_idloc,reg_idsens,reg_valor,reg_fechahora) VALUES(".$_GET['iduser'].",".$_GET['idloc'].",".$_GET['idsens2'].",".$_GET['valor2'].",CURRENT_TIMESTAMP)";
+	  mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); 
+	  $res = mysqli_query($mysqli,$sentencia);
+	  $res2 = mysqli_query($mysqli,$sentencia2);
+	  echo "yay";
+      }
+      else{
+	  include 'includes/configuracion.php';
+	  $sentencia = "INSERT INTO registros (reg_iduser,reg_idloc,reg_idsens,reg_valor,reg_fechahora) VALUES(".$_GET['iduser'].",".$_GET['idloc'].",".$_GET['idsens'].",".$_GET['valor'].",CURRENT_TIMESTAMP)";
+	  mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); 
+	  $res = mysqli_query($mysqli,$sentencia);
+	  echo "yay";
+	  }
     }
   }
 ?>
