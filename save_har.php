d66230a3feb3cc637e8a294dbc0475bdf378f14e
jemanjarres <jemanjarres@092634-ddocent-o.local>
2017-08-02 09:29:22 -0500

    primera tabla de HAR

--- save_har.php
+++ save_har.php
@@ -5,7 +5,7 @@ echo "welcome";
     if(isset($_GET['x']) && isset($_GET['y']) && isset($_GET['z'])){
     echo "segundo if";
 	  include 'includes/configuracion_har.php';
-	  //date_default_timezone_set('America/Bogota');
+	  date_default_timezone_set('America/Bogota');
 	  $sentencia = "INSERT INTO acelerometro (x,y,z,t) VALUES(".$_GET['x'].",".$_GET['y'].",".$_GET['z'].",CURRENT_TIMESTAMP)";
 	  mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); 
 	  $res = mysqli_query($mysqli,$sentencia);
