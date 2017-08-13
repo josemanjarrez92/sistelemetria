d0d38d5ef04220b1f66b3ff3c22bf315b1d8f1a4
Jose Manjarres <ASUS-H110M-R@ASUS-H110M-R.ASUS-H110M-R>
2017-08-13 10:56:40 -0500

    lo del aerogenerador

--- save_aero.php
+++ save_aero.php
@@ -2,7 +2,7 @@
 echo "welcome \n"; //echo es para mostrar texto en la página. En este caso mostramos texto para saber en qué etapa del algoritmo vamos.
 	if($_GET['type'] == 'acc'){
 		if(!empty($_GET['x']) && !empty($_GET['y']) && !empty($_GET['z'])){ //las variables que llegan por GET se pueden llamar utilizando $_GET['nombredevariable'].
-		echo "no están vacías \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
+		echo "no estan vacias \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
 		date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
@@ -10,19 +10,19 @@ echo "welcome \n"; //echo es para mostrar texto en la página. En este caso most
 		// sirven para concatenar caracteres en php.
 		mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); //configuramos la hora de la base de datos
 		$res = mysqli_query($mysqli,$sentencia); //ejectuamos el query en la base de datos seleccionada. Si todo sale bien, la variable $res será 'true', sino será 'false'
-		if($res){echo "Ya guardó!";} //si $res es 'true', decimos que ya guardó.
+		if($res){echo "Ya guardo!";} //si $res es 'true', decimos que ya guardó.
 		}
     }
 	elseif($_GET['type'] == 'rpm'){
 		if(!empty($_GET['rpm'])){ //las variables que llegan por GET se pueden llamar utilizando $_GET['nombredevariable'].
-		echo "no están vacías \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
+		echo "no estan vacias \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
 		date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
 		$sentencia = "INSERT INTO rpm (rpm,fecha) VALUES(".$_GET['rpm'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos sirven para concatenar caracteres en php.
 		mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); //configuramos la hora de la base de datos
 		$res = mysqli_query($mysqli,$sentencia); //ejectuamos el query en la base de datos seleccionada. Si todo sale bien, la variable $res será 'true', sino será 'false'
-		if($res){echo "Ya guardó!";} //si $res es 'true', decimos que ya guardó.
+		if($res){echo "Ya guardo!";} //si $res es 'true', decimos que ya guardó.
 		}
 	}
 ?>
