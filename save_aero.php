081422f7c4e1e842758236724c0d6501ca96e812
Jose Manjarres <ASUS-H110M-R@ASUS-H110M-R.ASUS-H110M-R>
2017-08-21 15:15:46 -0500

    cambios en la tabla de acelerometro

--- save_aero.php
+++ save_aero.php
@@ -1,12 +1,12 @@
 <?php // comienza el archivo php
 echo "welcome \n"; //echo es para mostrar texto en la página. En este caso mostramos texto para saber en qué etapa del algoritmo vamos.
 	if($_GET['type'] == 'acc'){
-		if(!empty($_GET['x']) && !empty($_GET['y']) && !empty($_GET['z'])){ //las variables que llegan por GET se pueden llamar utilizando $_GET['nombredevariable'].
+		if(!empty($_GET['maxx']) && !empty($_GET['mx']) && !empty($_GET['minx']) && !empty($_GET['maxy']) && !empty($_GET['my']) && !empty($_GET['miny']) && !empty($_GET['maxz']) && !empty($_GET['mz']) && !empty($_GET['minz'])){ //las variables que llegan por GET se pueden llamar utilizando $_GET['nombredevariable'].
 		echo "no estan vacias \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
 		date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
-		$sentencia = "INSERT INTO acelerometro (eje_x,eje_y,eje_z,fecha_hora) VALUES(".$_GET['x'].",".$_GET['y'].",".$_GET['z'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos
+		$sentencia = "INSERT INTO acelerometro (max_ejex,m_ejex,min_ejex,max_ejey,m_ejey,min_ejey,max_ejez,m_ejez,min_ejez,fecha_hora) VALUES(".$_GET['maxx'].",".$_GET['mx'].",".$_GET['minx'].",".$_GET['maxy'].",".$_GET['my'].",".$_GET['miny'].",".$_GET['maxz'].",".$_GET['mz'].",".$_GET['minz'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos
 		// sirven para concatenar caracteres en php.
 		mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); //configuramos la hora de la base de datos
 		$res = mysqli_query($mysqli,$sentencia); //ejectuamos el query en la base de datos seleccionada. Si todo sale bien, la variable $res será 'true', sino será 'false'
