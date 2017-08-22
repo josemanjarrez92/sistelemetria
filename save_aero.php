084566dbd3f3fc00748cad51b08706918f1b2951
jemanjarres <jemanjarres@092634-ddocent-o.local>
2017-08-22 15:31:11 -0500

    Correccion hora

--- save_aero.php
+++ save_aero.php
@@ -6,10 +6,10 @@ echo "welcome \n"; //echo es para mostrar texto en la página. En este caso most
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
 		//date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
-		$sentencia = "INSERT INTO acelerometro (max_ejex,m_ejex,min_ejex,max_ejey,m_ejey,min_ejey,max_ejez,m_ejez,min_ejez,fecha_hora) VALUES(".$_GET['maxx'].",".$_GET['mx'].",".$_GET['minx'].",".$_GET['maxy'].",".$_GET['my'].",".$_GET['miny'].",".$_GET['maxz'].",".$_GET['mz'].",".$_GET['minz'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos
+		$sentencia = "SET time_zone='-05:00';INSERT INTO acelerometro (max_ejex,m_ejex,min_ejex,max_ejey,m_ejey,min_ejey,max_ejez,m_ejez,min_ejez,fecha_hora) VALUES(".$_GET['maxx'].",".$_GET['mx'].",".$_GET['minx'].",".$_GET['maxy'].",".$_GET['my'].",".$_GET['miny'].",".$_GET['maxz'].",".$_GET['mz'].",".$_GET['minz'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos
 		// sirven para concatenar caracteres en php.
-		$cambio_hora = mysqli_query($mysqli,"SET time_zone='-05:00'"); //configuramos la hora de la base de datos
-		if($cambio_hora){echo "cambio la hora";}
+		//$cambio_hora = mysqli_query($mysqli,"SET time_zone='-05:00'"); //configuramos la hora de la base de datos
+		//if($cambio_hora){echo "cambio la hora";}
 		$res = mysqli_query($mysqli,$sentencia); //ejectuamos el query en la base de datos seleccionada. Si todo sale bien, la variable $res será 'true', sino será 'false'
 		if($res){echo "Ya guardo!";} //si $res es 'true', decimos que ya guardó.
 		}
