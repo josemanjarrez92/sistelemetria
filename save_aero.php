b53321f3b4b5b2c603acf0500844f095652b1f35
jemanjarres <jemanjarres@092634-ddocent-o.local>
2017-08-22 15:11:48 -0500

    Correccion hora

--- save_aero.php
+++ save_aero.php
@@ -5,7 +5,7 @@ echo "welcome \n"; //echo es para mostrar texto en la página. En este caso most
 		echo "no estan vacias \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
-		date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
+		//date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
 		$sentencia = "INSERT INTO acelerometro (max_ejex,m_ejex,min_ejex,max_ejey,m_ejey,min_ejey,max_ejez,m_ejez,min_ejez,fecha_hora) VALUES(".$_GET['maxx'].",".$_GET['mx'].",".$_GET['minx'].",".$_GET['maxy'].",".$_GET['my'].",".$_GET['miny'].",".$_GET['maxz'].",".$_GET['mz'].",".$_GET['minz'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos
 		// sirven para concatenar caracteres en php.
 		mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); //configuramos la hora de la base de datos
@@ -18,7 +18,7 @@ echo "welcome \n"; //echo es para mostrar texto en la página. En este caso most
 		echo "no estan vacias \n"; //En efecto, la función empty nos ayuda a verificar si una variable está vacía. Utilizar !empty nos ayuda a verificar lo contrario.
 		include 'includes/configuracion_aero.php'; //El archivo configuracion_aero contiene la definición de una variable llamada mysqli. Esta variable
 		//contiene toda la información necesaria para conectarse a la base de datos llamada aerogenerador
-		date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
+		//date_default_timezone_set('America/Bogota'); //configuramos la hora del servidor
 		$sentencia = "INSERT INTO rpm (rpm,fecha) VALUES(".$_GET['rpm'].",CURRENT_TIMESTAMP)"; //preparamos el query. Los puntos sirven para concatenar caracteres en php.
 		mysqli_query($mysqli,"SET SESSION time_zone = '-5:00'"); //configuramos la hora de la base de datos
 		$res = mysqli_query($mysqli,$sentencia); //ejectuamos el query en la base de datos seleccionada. Si todo sale bien, la variable $res será 'true', sino será 'false'
